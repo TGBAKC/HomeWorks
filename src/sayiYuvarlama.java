eabbc7d510425035206a02f5c67b73b9aad75a31
Tugba23 <filizakca4@gmail.com>
2025-07-23 20:38:20 +0200

    Merge remote-tracking branch 'origin/master'

---
+++ src/sayiYuvarlama.java
@@ -0,0 +1,41 @@
+import java.util.Scanner;
+
+public class sayiYuvarlama {
+
+
+    public static void main(String[] args) {
+
+        System.out.println("ondalikli bir sayi giriniz");
+        Scanner scanner = new Scanner(System.in);
+        double myDouble = scanner.nextDouble();
+        int myInt = (int) myDouble;
+
+        System.out.println(myDouble);
+        System.out.println(myInt);
+
+
+
+
+
+
+
+
+
+
+
+
+
+    }
+
+
+
+
+
+
+
+
+
+
+
+
+}
